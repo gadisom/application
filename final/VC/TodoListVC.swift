43c57cfca5931e39b1f1140692f1f011cade6aee
gadisom <jeongwon.kim64@gmail.com>
2023-06-06 03:18:19 +0900

    feat : Todolist 기능추가

---
+++ final/VC/TodoListVC.swift
@@ -0,0 +1,29 @@
+//
+//  TodoListVC.swift
+//  final
+//
+//  Created by 김정원 on 2023/06/05.
+//
+
+import UIKit
+
+class TodoListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ final/View/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  final
+//
+//  Created by 김정원 on 2023/06/05.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
